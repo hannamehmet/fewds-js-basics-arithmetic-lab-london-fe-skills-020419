1d8ca58fe1b1d614e219f6613d79533aaf86f6b8
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-10 08:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+const newID = 1000000000 + oldID;
+
+Number.isInteger(currentAge)
+var ageIsValid = true; if (currentAge.isInterger);
+
+Math.random() * 20; 
